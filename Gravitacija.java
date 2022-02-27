8f90572f8e9893bde6742243e4aba885aa5bde06
mtaseva <99970406+mtaseva@users.noreply.github.com>
2022-02-27 18:39:05 +0100

    Update Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -18,10 +18,10 @@ Scanner sc = new Scanner(System.in);
 System.out.println("Vnesete visina ");
 int visina = sc.nextInt();
 System.out.printf("Pospesek je enak %f", Gravitacija.pospesek(visina));
-
+}
+  
 public static void izpis(double pospesek, int visina) {
   System.out.printf("Pospesek je enak %f\n", pospesek);
   System.out.printf("Nadmorska visina je enaka %d\n, visina")
-}
 }
 }
